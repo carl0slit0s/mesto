9a0cecf9c97c21762d2a6c5060ea664806d86731
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2022-01-16 17:24:06 +0500

    script chenge let on const

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-let profileEditButton = document.querySelector(".profile__edit");
-let redactorProfile = document.querySelector(".popup");
+const profileEditButton = document.querySelector(".profile__edit");
+const redactorProfile = document.querySelector(".popup");
 // let redactorProfileSaveButton = document.querySelector(".edit-form__save");
-let profileEditClosedButton = document.querySelector(".edit-form__close-icon");
+const profileEditClosedButton = document.querySelector(".edit-form__close-icon");
 
-let editProfileName = document.querySelector(".edit-form__name");
-let editProfileAbout = document.querySelector(".edit-form__about");
+const editProfileName = document.querySelector(".edit-form__name");
+const editProfileAbout = document.querySelector(".edit-form__about");
 
-let card_likes = document.querySelectorAll(".photo-card__like");
+const card_likes = document.querySelectorAll(".photo-card__like");
 
-let redactorProfileForm = document.querySelector(".edit-form");
+const redactorProfileForm = document.querySelector(".edit-form");
 
 // закрыть popup
 function openRedactorProfile(event) {
   event.preventDefault();
 
-  let profileName = document.querySelector(".profile__name").textContent;
-  let profileAbout = document.querySelector(".profile__about").textContent;
+  const profileName = document.querySelector(".profile__name").textContent;
+  const profileAbout = document.querySelector(".profile__about").textContent;
 
   redactorProfile.classList.add("popup_opened");
   editProfileName.value = profileName;
@@ -36,8 +36,8 @@ function saveRedactorProfile(event) {
   event.preventDefault();
   redactorProfile.classList.remove("popup_opened");
 
-  let inputName = document.querySelector(".edit-form__name").value;
-  let inputAbout = document.querySelector(".edit-form__about").value;
+  const inputName = document.querySelector(".edit-form__name").value;
+  const inputAbout = document.querySelector(".edit-form__about").value;
 
   document.querySelector(".profile__name").textContent = inputName;
   document.querySelector(".profile__about").textContent = inputAbout;
