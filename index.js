d3f2aae593e8e1cf9f7d42befdb40719ef438003
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2022-01-17 20:15:04 +0500

    исправлены замечания

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
 const profileEditButton = document.querySelector(".profile__edit");
 const redactorProfile = document.querySelector(".popup");
-// let redactorProfileSaveButton = document.querySelector(".edit-form__save");
-const profileEditClosedButton = document.querySelector(".edit-form__close-icon");
+const profileEditClosedButton = document.querySelector(
+  ".edit-form__close-icon"
+);
 
-const editProfileName = document.querySelector(".edit-form__name");
-const editProfileAbout = document.querySelector(".edit-form__about");
-
-const card_likes = document.querySelectorAll(".photo-card__like");
+const inputProfileName = document.querySelector(".edit-form__name");
+const inputProfileAbout = document.querySelector(".edit-form__about");
 
 const redactorProfileForm = document.querySelector(".edit-form");
 
-// закрыть popup
+const profileName = document.querySelector(".profile__name");
+const profileAbout = document.querySelector(".profile__about");
+
+// открыть popup
 function openRedactorProfile(event) {
   event.preventDefault();
 
-  const profileName = document.querySelector(".profile__name").textContent;
-  const profileAbout = document.querySelector(".profile__about").textContent;
-
   redactorProfile.classList.add("popup_opened");
-  editProfileName.value = profileName;
-  editProfileAbout.value = profileAbout;
 
-  profileEditClosedButton.addEventListener("click", closedRedactorProfile);
-  redactorProfileForm.addEventListener("submit", saveRedactorProfile);
+  inputProfileName.value = profileName.textContent;
+  inputProfileAbout.value = profileAbout.textContent;
 }
 
 // Закрыть popup
 function closedRedactorProfile(event) {
-  event.preventDefault();
   redactorProfile.classList.remove("popup_opened");
 }
 
 // сохранить изменения
 function saveRedactorProfile(event) {
   event.preventDefault();
-  redactorProfile.classList.remove("popup_opened");
-
-  const inputName = document.querySelector(".edit-form__name").value;
-  const inputAbout = document.querySelector(".edit-form__about").value;
-
-  document.querySelector(".profile__name").textContent = inputName;
-  document.querySelector(".profile__about").textContent = inputAbout;
+  closedRedactorProfile();
+  profileName.textContent = inputProfileName.value;
+  profileAbout.textContent = inputProfileAbout.value;
 }
 
 profileEditButton.addEventListener("click", openRedactorProfile);
+profileEditClosedButton.addEventListener("click", closedRedactorProfile);
+redactorProfileForm.addEventListener("submit", saveRedactorProfile);
